45b64cadf9e9f1a95ef0e0dc275272347296c487
metsawyr <metsawyr@gmail.com>
2024-05-27 19:40:32 +0000

    fix: edit sha hashsum

--- nix/config/wsl.nix
+++ nix/config/wsl.nix
@@ -8,7 +8,7 @@
   	inputs.nixos-wsl.nixosModules.wsl
 	(fetchTarball {
 		url = "https://github.com/nix-community/nixos-vscode-server/tarball/master";
-		sha256 = "";
+		sha256 = "1rq8mrlmbzpcbv9ys0x88alw30ks70jlmvnfr2j8v830yy5wvw7h";
 	})
   ];
 
