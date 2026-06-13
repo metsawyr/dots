c99ce4a9b9c8cfcaddde44f6824eac9bfcc2230c
metsawyr <metsawyr@gmail.com>
2026-06-13 13:26:01 +0000

    fix: lazy

--- nix/home/programs/neovim/default.nix
+++ nix/home/programs/neovim/default.nix
@@ -6,12 +6,10 @@
 }: {
   home.packages = with pkgs; [
     neovim
-    vimPlugins.lazy-nvim
   ];
 
   home.sessionVariables = {
     EDITOR = "nvim";
-    NIXPKGS_LAZYNVIM = pkgs.vimPlugins.lazy-nvim;
   };
 
   xdg.configFile = with config.lib.file; {
