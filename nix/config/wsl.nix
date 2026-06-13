327a6cd296fabb9164729ddbe4872659645c20d4
metsawyr <metsawyr@gmail.com>
2026-06-13 10:36:17 +0200

    feat: wsl graphics

--- nix/config/wsl.nix
+++ nix/config/wsl.nix
@@ -11,12 +11,21 @@
 
   environment.systemPackages = [
     (import ./win32yank.nix {inherit pkgs;})
+    pkgs.vulkan-tools # vkcube, vulkaninfo
+    pkgs.vulkan-loader
+    pkgs.vulkan-validation-layers
+    pkgs.mesa-demos # glxgears sanity check
   ];
 
   wsl = {
     enable = true;
     defaultUser = user;
+    # Symlink Windows GPU driver into /usr/lib/wsl -> hw-accel GL/Vulkan via WSLg.
+    useWindowsDriver = true;
   };
+
+  # Enable graphics stack (Mesa + Vulkan loader).
+  hardware.graphics.enable = true;
 
   services.vscode-server.enable = true;
   services.vscode-server.enableFHS = true;
