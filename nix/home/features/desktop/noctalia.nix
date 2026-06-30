db753e8210fed35c782a09c91eb8314cc298e91c
metsawyr <metsawyr@gmail.com>
2026-06-30 18:49:25 +0200

    fix: derivation hook

--- nix/home/features/desktop/noctalia.nix
+++ nix/home/features/desktop/noctalia.nix
@@ -9,7 +9,7 @@ lib.mkIf config.dev.desktop.hyprland.enable {
   programs.noctalia-shell = {
     enable = true;
     package = (inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default).overrideAttrs (old: {
-      postInstall = (old.postInstall or "") + ''
+      installPhase = old.installPhase + ''
         cp ${./SystemStatsPanel.qml} $out/share/noctalia-shell/Modules/Panels/SystemStats/SystemStatsPanel.qml
       '';
     });
