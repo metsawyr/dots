2cdba5d4bb7152732f522161a64217d33ab44fab
metsawyr <metsawyr@gmail.com>
2026-06-30 18:30:50 +0200

    feat: display all partitions in status widget

--- nix/home/features/desktop/noctalia.nix
+++ nix/home/features/desktop/noctalia.nix
@@ -1,8 +1,17 @@
 {
   lib,
   config,
+  pkgs,
+  inputs,
   ...
 }:
 lib.mkIf config.dev.desktop.hyprland.enable {
-  programs.noctalia-shell.enable = true;
+  programs.noctalia-shell = {
+    enable = true;
+    package = (inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default).overrideAttrs (old: {
+      postInstall = (old.postInstall or "") + ''
+        cp ${./SystemStatsPanel.qml} $out/share/noctalia-shell/Modules/Panels/SystemStats/SystemStatsPanel.qml
+      '';
+    });
+  };
 }
