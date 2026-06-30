09997571784f9179d9eac3ee997c764c637df1f2
metsawyr <metsawyr@gmail.com>
2026-06-30 19:52:46 +0200

    feat: make partitions status as patch

--- nix/home/features/desktop/noctalia.nix
+++ nix/home/features/desktop/noctalia.nix
@@ -9,9 +9,7 @@ lib.mkIf config.dev.desktop.hyprland.enable {
   programs.noctalia-shell = {
     enable = true;
     package = (inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default).overrideAttrs (old: {
-      installPhase = old.installPhase + ''
-        cp ${./SystemStatsPanel.qml} $out/share/noctalia-shell/Modules/Panels/SystemStats/SystemStatsPanel.qml
-      '';
+      patches = (old.patches or []) ++ [ ./system-stats-panel-disk-paths.patch ];
     });
   };
 }
